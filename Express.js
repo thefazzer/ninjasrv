a87028cfabc4b3d9e2b53fa64db6c0e1b13dd8fa
Paul Farrington <pfarrington@gmail.com>
2023-07-14 20:17:48 +0100

    express-session include, code rewrite.

--- Express.js
+++ Express.js
@@ -1,6 +1,15 @@
-
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const express = require('express');
+const session = require('express-session');
+
+const app = express();
+const port = 3000;
+
+app.use(session({ secret: 'secretKey', resave: false, saveUninitialized: false }));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 passport.use(new GoogleStrategy({
   clientID: '740807273849-h1btj8ui5fkdvq14a9ulnl601ukbq6p0.apps.googleusercontent.com',
@@ -8,61 +17,49 @@ passport.use(new GoogleStrategy({
   callbackURL: "http://www.example.com/auth/google/callback"
 },
 function(accessToken, refreshToken, profile, cb) {
-  // Here you would typically look up the user in your database using the profile info
-  // And perform your admin check
-  // For this dummy example, let's say every Google user is an admin
+  profile.isAdmin = true; // Add isAdmin flag to the profile
   return cb(null, profile);
 }
 ));
 
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
 
-const express = require('express');
-const app = express();
-const port = 3000;
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
 
-
-// Here's a route that triggers the Google authentication
 app.get('/auth/google',
   passport.authenticate('google', { scope: ['profile'] }));
 
-// And here's the callback route Google will redirect the user to after they have signed in
 app.get('/auth/google/callback', 
   passport.authenticate('google', { failureRedirect: '/login' }),
   function(req, res) {
-    // Successful authentication, redirect home.
+    req.session.isAdmin = req.user.isAdmin;
     res.redirect('/');
   });
 
+function isAdmin(req, res, next) {
+  if (req.session.isAdmin) {
+    next();
+  } else {
+    res.status(403).send('You are not authorized to perform this action');
+  }
+}
 
 let dummyServiceStatus = 'stopped';
 
-// Middleware for checking if the user is admin
-function isAdmin(req, res, next) {
-  // TODO: Implement your authentication logic here.
-  // This is just a placeholder
-  next();
-  //if (req.get('Authorization') === 'Bearer admin') {
-  //  next();
-  //} else {
-  //res.status(403).send('You are not authorized to perform this action');
-  //}
-}
-
-// Status endpoint
 app.get('/status', isAdmin, (req, res) => {
   res.send(`Dummy service is currently: ${dummyServiceStatus}`);
 });
 
-// Start endpoint
 app.post('/start', isAdmin, (req, res) => {
-  // TODO: Implement your service start logic here
   dummyServiceStatus = 'running';
   res.send('Dummy service started');
 });
 
-// Stop endpoint
 app.post('/stop', isAdmin, (req, res) => {
-  // TODO: Implement your service stop logic here
   dummyServiceStatus = 'stopped';
   res.send('Dummy service stopped');
 });
@@ -70,4 +67,3 @@ app.post('/stop', isAdmin, (req, res) => {
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
 });
-
